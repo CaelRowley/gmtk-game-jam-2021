3822e3d33a5ebd17945e782c24b4558cd6615051
caelrowley <caelrowley@gmail.com>
2021-06-12 15:16:25 +0200

    feat: add player goal barrier

---
+++ src/World/Barrier.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+onready var player1Goal = $Area2D
+
+var player1InGoal = false
+var player2InGoal = false
+
+func createGrassEffect():
+	var grassEffect = GrassEffect.instance()
+	get_parent().add_child(grassEffect)
+	grassEffect.global_position = global_position
+	queue_free()
+
+
+func _on_Hurtbox_area_entered(_area):
+	createGrassEffect()
+	
+func _process(delta):
+	if(player1InGoal && player2InGoal):
+		createGrassEffect()
+
+
+func _on_Area2D_body_entered(body):
+	print(body.get_name(), ' entered')
+	if(body.get_name() == 'Player1'):
+		print('player1InGoal = true')
+		player1InGoal = true
+	if(body.get_name() == 'Player2'):
+		print('player2InGoal = true')
+		player2InGoal = true
+
+func _on_Area2D_body_exited(body):
+	print(body.get_name(), ' left')
+	if(body.get_name() == 'Player1'):
+		print('player1InGoal = false')
+		player1InGoal = false
+	if(body.get_name() == 'Player2'):
+		print('player2InGoal = false')
+		player2InGoal = false

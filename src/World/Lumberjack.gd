0abd9dbee825d48b0c864d8390cfd97b3435cec4
caelrowley <caelrowley@gmail.com>
2021-06-13 13:23:27 +0200

    feat: breaker of chains

--- src/World/Lumberjack.gd
+++ src/World/Lumberjack.gd
@@ -9,6 +9,8 @@ var velocity = Vector2.ZERO
 const MAX_SPEED = 100
 const ACCELERATION = 400
 
+var hasPlayedAudio = false
+
 func _process(delta):
 	if(player1InGoal && player2InGoal):
 		$AnimatedSprite.animation =  "Attack"
@@ -16,6 +18,10 @@ func _process(delta):
 	
 		velocity = velocity.move_toward(Vector2.RIGHT * MAX_SPEED, ACCELERATION * delta)
 		velocity = $KinematicBody2D.move_and_slide(velocity)
+		if(!hasPlayedAudio):
+			hasPlayedAudio = true
+			$AudioStreamPlayer.play()
+			$AudioStreamPlayer.stop()
 	else:
 		$AnimatedSprite.animation =  "Idle"
 		$KinematicBody2D/Area2D/CollisionShape2D.set_deferred("disabled", true)
@@ -30,6 +36,7 @@ func _on_Area2D_body_entered(body):
 
 
 func _on_Area2D_body_exited(body):
+	hasPlayedAudio = false
 	if(body.get_name() == 'Player1'):
 		print('player1InGoal = false')
 		player1InGoal = false
